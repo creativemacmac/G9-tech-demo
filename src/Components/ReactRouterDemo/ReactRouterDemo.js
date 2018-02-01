5de783b40ee79aff2229429a17797788cb2765f0
Fabrice Toussaint <ftoussaint@groupninemedia.com>
2018-02-01 18:03:10 -0500

    basic two seperate react router demos

--- src/Components/ReactRouterDemo/ReactRouterDemo.js
+++ src/Components/ReactRouterDemo/ReactRouterDemo.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { Link, Switch, Route } from 'react-router-dom';
 
 // Styles
-import { Header, Menu } from 'semantic-ui-react';
+import { Header, Menu, Dropdown } from 'semantic-ui-react';
 import './__styles__/index.css';
 
 // Components
@@ -29,16 +29,34 @@ class ReactRouterDemo extends React.Component<{}, State> {
 
         return (
             <div>
-                <Header as='h2' content='Demo using React Router 4 with 4 nested routes' dividing />
+                <Header as='h2' content='Demo using React Router 4' dividing />
 
                 <Menu secondary vertical className='demo-sidebar'>
                     <Menu.Item as={Link} to='/reactRouter/demo1' name='Demo 1: Blocking page transitions' active={activeItem === 'Demo 1: Blocking page transitions'} onClick={this.handleItemClick} />
-                    <Menu.Item as={Link} to='/reactRouter/demo2' name='Demo 2: Params and ids' active={activeItem === 'Demo 2: Params and ids'} onClick={this.handleItemClick} />
+                    {/* <Menu.Item as={Link} to='/reactRouter/demo2' name='Demo 2: Params and ids' active={activeItem === 'Demo 2: Params and ids'} onClick={this.handleItemClick} /> */}
+                    <Dropdown item text='Demo 2: (ids) Alien Workshop Mind Field Team'>
+                        <Dropdown.Menu>
+                            <Dropdown.Header>Parts in MindField</Dropdown.Header>
+                            <Dropdown.Item as={Link} to='/reactRouter/demo2/0'>Heath Kirchart</Dropdown.Item>
+                            <Dropdown.Item as={Link} to='/reactRouter/demo2/1'>Arto Saari</Dropdown.Item>
+                            <Dropdown.Item as={Link} to='/reactRouter/demo2/2'>Mikey Taylor</Dropdown.Item>
+                            <Dropdown.Item as={Link} to='/reactRouter/demo2/3'>AVE</Dropdown.Item>
+                            <Dropdown.Item as={Link} to='/reactRouter/demo2/4'>Josh Kalis</Dropdown.Item>
+                            <Dropdown.Item as={Link} to='/reactRouter/demo2/5'>Dylan Reider (RIP)</Dropdown.Item>
+                            <Dropdown.Item as={Link} to='/reactRouter/demo2/6'>Omar Salazar</Dropdown.Item>
+                            <Dropdown.Item as={Link} to='/reactRouter/demo2/7'>Jason Dill</Dropdown.Item>
+                            <Dropdown.Item as={Link} to='/reactRouter/demo2/8'>Jake Johnson</Dropdown.Item>
+                            <Dropdown.Item as={Link} to='/reactRouter/demo2/9'>Tyler Bledsoe</Dropdown.Item>
+                            <Dropdown.Item as={Link} to='/reactRouter/demo2/10'>Grant Taylor</Dropdown.Item>
+                            <Dropdown.Item as={Link} to='/reactRouter/demo2/11'>Rob Dyrdek</Dropdown.Item>
+                            <Dropdown.Item as={Link} to='/reactRouter/demo2/12'>Steve Berra</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </Menu>
 
                 <Switch>
                     <Route path='/reactRouter/demo1' component={Demo1} />
-                    <Route path='/reactRouter/demo2' component={Demo2} />
+                    <Route path='/reactRouter/demo2/:id' component={Demo2} />
                 </Switch>
 
             </div>
---
+++ src/Components/ReactRouterDemo/subComponents/Demo2API.js
@@ -0,0 +1,19 @@
+const Demo2API = {
+  skaters: [
+    { id: 0, name: ' Heath Kirchart ', videoId: '5ZdsdeY5N40' },
+    { id: 1, name: ' Arto Saari ', videoId: 'tKIzaMdo3Pg' },
+    { id: 2, name: ' Mikey Taylor ', videoId: 'UyHdo4wzxbQ' },
+    { id: 3, name: ' AVE ', videoId: 'OgiLPqJGnhs' },
+    { id: 4, name: ' Josh Kalis ', videoId: 'G8Q4xwI7ieA' },
+    { id: 5, name: ' Dylan Reider (RIP) ', videoId: 'qJZjRzH2EVo' },
+    { id: 6, name: ' Omar Salazar ', videoId: 'EfZeGhnLdUM' },
+    { id: 7, name: ' Jason Dill ', videoId: 'lLjtC_kU0JY' },
+    { id: 8, name: ' Jake Johnson ', videoId: '3gx4PKwSfNY' },
+    { id: 9, name: ' Tyler Bledsoe ', videoId: 'Ab8hkjUiT7c' },
+    { id: 10, name: ' Grant Taylor ', videoId: '_vjRbuL0y9o' },
+    { id: 11, name: ' Rob Dyrdek ', videoId: 'avW_hBMsYvg' },
+    { id: 12, name: ' Steve Berra ', videoId: 'gC0nKbMGqr8' },
+  ],
+};
+
+export default Demo2API;
