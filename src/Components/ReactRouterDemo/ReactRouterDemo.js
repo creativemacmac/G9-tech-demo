a6dcf8126e64cafc5fcea4e8fb1ee48c7e93595c
Fabrice Toussaint <ftoussaint@groupninemedia.com>
2018-02-02 13:53:47 -0500

    remove comment

--- src/Components/ReactRouterDemo/ReactRouterDemo.js
+++ src/Components/ReactRouterDemo/ReactRouterDemo.js
@@ -34,23 +34,22 @@ class ReactRouterDemo extends React.Component<{}, State> {
 
                 <Menu secondary vertical className='demo-sidebar'>
                     <Menu.Item as={Link} to='/reactRouter/demo1' name='Demo 1: Blocking page transitions' active={activeItem === 'Demo 1: Blocking page transitions'} onClick={this.handleItemClick} />
-                    {/* <Menu.Item as={Link} to='/reactRouter/demo2' name='Demo 2: Params and ids' active={activeItem === 'Demo 2: Params and ids'} onClick={this.handleItemClick} /> */}
                     <Dropdown item text='Demo 2: (ids) Alien Workshop Mind Field Team'>
                         <Dropdown.Menu>
                             <Dropdown.Header>Parts in MindField</Dropdown.Header>
-                            <Dropdown.Item as={Link} to='/reactRouter/demo2/0'>Heath Kirchart</Dropdown.Item>
-                            <Dropdown.Item as={Link} to='/reactRouter/demo2/1'>Arto Saari</Dropdown.Item>
-                            <Dropdown.Item as={Link} to='/reactRouter/demo2/2'>Mikey Taylor</Dropdown.Item>
-                            <Dropdown.Item as={Link} to='/reactRouter/demo2/3'>AVE</Dropdown.Item>
-                            <Dropdown.Item as={Link} to='/reactRouter/demo2/4'>Josh Kalis</Dropdown.Item>
-                            <Dropdown.Item as={Link} to='/reactRouter/demo2/5'>Dylan Reider (RIP)</Dropdown.Item>
-                            <Dropdown.Item as={Link} to='/reactRouter/demo2/6'>Omar Salazar</Dropdown.Item>
-                            <Dropdown.Item as={Link} to='/reactRouter/demo2/7'>Jason Dill</Dropdown.Item>
-                            <Dropdown.Item as={Link} to='/reactRouter/demo2/8'>Jake Johnson</Dropdown.Item>
-                            <Dropdown.Item as={Link} to='/reactRouter/demo2/9'>Tyler Bledsoe</Dropdown.Item>
-                            <Dropdown.Item as={Link} to='/reactRouter/demo2/10'>Grant Taylor</Dropdown.Item>
-                            <Dropdown.Item as={Link} to='/reactRouter/demo2/11'>Rob Dyrdek</Dropdown.Item>
-                            <Dropdown.Item as={Link} to='/reactRouter/demo2/12'>Steve Berra</Dropdown.Item>
+                            <Dropdown.Item as={Link} to={`${this.props.match.path}/demo2/0`}>Heath Kirchart</Dropdown.Item>
+                            <Dropdown.Item as={Link} to={`${this.props.match.path}/demo2/1`}>Arto Saari</Dropdown.Item>
+                            <Dropdown.Item as={Link} to={`${this.props.match.path}/demo2/2`}>Mikey Taylor</Dropdown.Item>
+                            <Dropdown.Item as={Link} to={`${this.props.match.path}/demo2/3`}>AVE</Dropdown.Item>
+                            <Dropdown.Item as={Link} to={`${this.props.match.path}/demo2/4`}>Josh Kalis</Dropdown.Item>
+                            <Dropdown.Item as={Link} to={`${this.props.match.path}/demo2/5`}>Dylan Reider (RIP)</Dropdown.Item>
+                            <Dropdown.Item as={Link} to={`${this.props.match.path}/demo2/6`}>Omar Salazar</Dropdown.Item>
+                            <Dropdown.Item as={Link} to={`${this.props.match.path}/demo2/7`}>Jason Dill</Dropdown.Item>
+                            <Dropdown.Item as={Link} to={`${this.props.match.path}/demo2/8`}>Jake Johnson</Dropdown.Item>
+                            <Dropdown.Item as={Link} to={`${this.props.match.path}/demo2/9`}>Tyler Bledsoe</Dropdown.Item>
+                            <Dropdown.Item as={Link} to={`${this.props.match.path}/demo2/10`}>Grant Taylor</Dropdown.Item>
+                            <Dropdown.Item as={Link} to={`${this.props.match.path}/demo2/11`}>Rob Dyrdek</Dropdown.Item>
+                            <Dropdown.Item as={Link} to={`${this.props.match.path}/demo2/12`}>Steve Berra</Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
                 </Menu>
